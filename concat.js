9a0f69df9b6c6d45141469591678cdb817ad6c7b
Abdullah Sajal <59488317+sajal-se@users.noreply.github.com>
2024-01-30 13:52:40 +0200

    Concat two array into a new array

---
+++ concat.js
@@ -0,0 +1,11 @@
+// ### 5. Combining Arrays
+// **Instructions:**
+// 1. Create two arrays of your choice.
+// 2. Use the concat method to combine the two arrays into a new array.
+// 3. Print both the original arrays and the combined array using console.log().
+
+let visitedCountry = ["bangladesh", "india", "bidesh"];
+let touristDestination = ["Norway", "Sweden", "Finland"];
+
+let tours = visitedCountry.concat(touristDestination);
+console.log(tours);
